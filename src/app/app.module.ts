b3f933a6a90668c2def71e24284262dc12a30ed3
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-12-29 11:53:48 +0100

    implements folders and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrazioneComponent } from './registrazione/registrazione.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrazioneComponent
   ],
   imports: [
     BrowserModule
