1f193b0080b1eb62d087fd41708e6bf8ad8954c7
Roccofranchini <rocco.franchini1@gmail.com>
2021-12-29 12:29:27 +0100

    login-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
 import { AppRoutingModule } from './modules/app-routing.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrazioneComponent
+    RegistrazioneComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
