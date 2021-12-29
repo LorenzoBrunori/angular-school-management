7083c4cefdc8457e95fe31f67b631ae3139651eb
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-12-29 11:56:08 +0100

    fixed router outlet

--- src/app/routes/app-routes.routes.ts
+++ src/app/routes/app-routes.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { AppComponent } from "../app.component";
+import { RegistrazioneComponent } from "../components/registrazione/registrazione.component";
 
 export const AppRoutes: Routes = [
     { path: '', component: AppComponent },
+    { path: 'registrazione', component: RegistrazioneComponent},
 
     { path: '**', component: AppComponent }
 ];
