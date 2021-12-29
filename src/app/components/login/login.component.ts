3d33b56111f3db5706a9d822c5bde69bffcb9441
Roccofranchini <rocco.franchini1@gmail.com>
2021-12-29 18:20:17 +0100

    workin-login-form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-login",
@@ -10,12 +12,13 @@ import { NgForm } from "@angular/forms";
 export class LoginComponent implements OnInit {
   @ViewChild("loginForm") loginForm: NgForm;
   submitted = false;
+  users = [];
   user = {
     email: "",
     password: "",
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   ngOnInit(): void {}
 
@@ -24,8 +27,38 @@ export class LoginComponent implements OnInit {
     this.user.email = this.loginForm.value.email;
     this.user.password = this.loginForm.value.password;
 
-    this.http.get("http://localhost:3000/users/1").subscribe((users) => {
-      console.log(users);
-    });
+    this.http
+      .get("http://localhost:3000/users")
+      .pipe(
+        map((responseData) => {
+          const usersArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              usersArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return usersArray;
+        })
+      )
+      .subscribe((users) => {
+        console.log(users);
+        this.users = users;
+
+        for (let i = 0; i < users.length; i++) {
+          if (
+            users[i].email == this.user.email &&
+            users[i].password == this.user.password
+          ) {
+            this.authService.login();
+            break;
+          } else {
+            console.log("ACCESSO FALLITO");
+          }
+        }
+      });
+  }
+
+  logOut() {
+    this.authService.logout();
   }
 }
